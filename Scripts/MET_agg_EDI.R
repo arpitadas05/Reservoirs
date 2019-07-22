5209007353eec2e5bdf54f5f4742ab63f56fb5c8
bethanybookout <bethanyb18@vt.edu>
2019-07-21 22:15:20 -0400

    time change fix for Met_now

--- Scripts/MET_agg_EDI.R
+++ Scripts/MET_agg_EDI.R
@@ -37,7 +37,10 @@ if(length(names(Met_now))>17){ #removes NR01TK_Avg column, which was downloaded 
 }
 #renames and reformats dataset for easy bind
 names(Met_now) = c("TIMESTAMP","RECORD","BattV","PTemp_C","PAR_Den_Avg","PAR_Tot_Tot","BP_kPa_Avg","AirTC_Avg","RH","Rain_mm_Tot","WS_ms_Avg","WindDir","SR01Up_Avg","SR01Dn_Avg","IR01UpCo_Avg","IR01DnCo_Avg","Albedo_Avg")
-Met_now$TIMESTAMP=ymd_hms(Met_now$TIMESTAMP, tz="Etc/GMT+5")
+met_timechange=max(which(Met_now$TIMESTAMP=="2019-04-15 10:19:00")) #shows time point when met station was switched from GMT -4 to GMT -5
+Met_now$TIMESTAMP<-as.POSIXct(strptime(Met_now$TIMESTAMP, "%Y-%m-%d %H:%M"), tz = "Etc/GMT+5") #get dates aligned
+Met_now$TIMESTAMP[c(1:met_timechange-1)]<-with_tz(force_tz(Met_now$TIMESTAMP[c(1:met_timechange-1)],"Etc/GMT+4"), "Etc/GMT+5") #pre time change data gets assigned proper timezone then corrected to GMT -5 to match the rest of the data set
+
 Met_now=Met_now[-c(which(is.na(Met_now$TIMESTAMP))),] #checks for NA from failed parse, and deletes from dataset
 Met_now$PAR_Tot_Tot=as.numeric(Met_now$PAR_Tot_Tot) #matching str to past data
 #str(Met_now); str(Met_past) #checks structure for matching of historical dataset with
