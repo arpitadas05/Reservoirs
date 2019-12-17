ee28c3902fbb10b95711ed3484e7e15d18ec4ecd
abbylewis <aslewis@vt.edu>
2019-12-17 15:13:52 -0500

    2019 CTD production files

--- Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEMLCTD/MakeEMLCTD.R
+++ Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEMLCTD/MakeEMLCTD.R
@@ -11,17 +11,26 @@ import_templates(path = "C:/Users/Owner/Dropbox/MakeEMLCTD",
                  license = "CCBY", 
                  data.files = "CTD_Meta_13_18_final.csv")
 
+
+#Geographic coverage
+template_geographic_coverage(path = getwd(),
+                             data.path = getwd(),
+                             data.table = "CTD_final_2013_2019.csv",
+                             empty = TRUE,
+                             write.file = TRUE)
+
+
 # Define Categorical Variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-define_catvars(path = "C:/Users/Owner/Dropbox/MakeEMLCTD")
+#template_categorical_variables(path = "C:/Users/Owner/Dropbox/MakeEMLCTD")
 
 # Make the EML for EDI ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-make_eml(path = "C:/Users/Owner/Dropbox/MakeEMLCTD",
+make_eml(path = getwd(),
          dataset.title = "Time series of high-frequency profiles of depth, temperature, dissolved oxygen, conductivity, specific conductivity, chlorophyll a, turbidity, pH, oxidation-reduction potential, photosynthetic active radiation, and descent rate for Beaverdam Reservoir, Carvins Cove Reservoir, Falling Creek Reservoir, Gatewood Reservoir, and Spring Hollow Reservoir in Southwestern Virginia, USA 2013-2018",
-         data.files = "CTD_Meta_13_18_final.csv",
-         data.files.description = "Reservoir CTD dataset",
-         data.files.quote.character = "\"",
-         temporal.coverage = c("2013-03-07", "2018-12-17"),
+         data.table = "CTD_final_2013_2019.csv",
+         data.table.description = c("Reservoir CTD dataset"),
+         temporal.coverage = c("2013-03-07", "2019-12-16"),
+         geographic.description = "Southwestern Virginia, USA, North America",
          maintenance.description = "ongoing",
-         affiliation = "EDI",
+         user.domain = "EDI",
          user.id = "ccarey",
-         package.id = "edi.200.6")
+         package.id = "edi.200.7")
